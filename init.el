a661dfcbcc8334a418a8743ee36b254d907d7ffa
Matheus <matheus.hs97@gmail.com>
2021-07-13 17:42:31 -0300

    Update init.el

--- init.el
+++ init.el
@@ -163,6 +163,13 @@
   (prettier-js-mode))
 (add-hook 'web-mode-hook  'web-mode-init-prettier-hook)
 (add-hook 'web-mode-hook  'emmet-mode)
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2)
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
 ;; </web-mode>
 
 ;; <custom_functions>
